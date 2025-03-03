e6c8fdd39f4b903d99f7128f0cea642735c84820
A'nsi Lema <ansilema@gmail.com>
2025-03-03 12:46:51 +0100

    Enforce booking limit of 12 places per competition
        Added server-side validation to restrict bookings to a maximum of 12 places per competition. Implemented error messaging with flash notifications to inform users of this restriction.

--- server.py
+++ server.py
@@ -46,6 +46,11 @@ def purchasePlaces():
     competition = [c for c in competitions if c['name'] == request.form['competition']][0]
     club = [c for c in clubs if c['name'] == request.form['club']][0]
     placesRequired = int(request.form['places'])
+
+    if placesRequired > 12:
+        flash("Error: You cannot book more than 12 places per competition", "error")
+        return redirect(url_for('book', competition=competition['name'], club=club['name']))
+
     competition['numberOfPlaces'] = int(competition['numberOfPlaces'])-placesRequired
     flash('Great-booking complete!')
     return render_template('welcome.html', club=club, competitions=competitions)
